15ed9e6dc5fff0591e39b12b2803e84d05e8e546
mhdbel <155534967+mhdbel@users.noreply.github.com>
2025-07-25 18:57:56 +0100

    test_api.py

--- Traffic Management System/tests/test_api.py
+++ Traffic Management System/tests/test_api.py
@@ -1,5 +1,5 @@
 import requests
 
 def test_predict_endpoint():
-    response = requests.post("http://127.0.0.1:5000/predict", json={"temp": 25})
+    response = requests.post("http://127.0.0.1:5000/predict", json={"origin": "Rabat", "destination": "Casablanca"})
     assert response.status_code == 200
